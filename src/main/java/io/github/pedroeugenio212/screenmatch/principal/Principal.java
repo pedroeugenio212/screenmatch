27d40e667eb2414ea64b32918d2a4c48e4920136
Pedro Eugênio <pedroeugenio212@gmail.com>
2025-08-01 19:04:14 -0300

    exercicio - estatísticas da série

--- src/main/java/io/github/pedroeugenio212/screenmatch/principal/Principal.java
+++ src/main/java/io/github/pedroeugenio212/screenmatch/principal/Principal.java
@@ -6,6 +6,7 @@ import io.github.pedroeugenio212.screenmatch.model.Episodio;
 import io.github.pedroeugenio212.screenmatch.service.ConsumoApi;
 import io.github.pedroeugenio212.screenmatch.service.ConverteDados;
 import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -79,8 +80,17 @@ public class Principal
 //		+ ", Data Lançamento: " + e.getDataLancamento().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
 //	));
 	Map<Integer, Double> avaliacoesPorTemporada = episodios.stream()
+		.filter(e -> e.getAvaliacao() > 0.0)
 		.collect(Collectors.groupingBy(Episodio::getTemporada,
 					       Collectors.averagingDouble(Episodio::getAvaliacao)));
 	System.out.println(avaliacoesPorTemporada);
+	
+	DoubleSummaryStatistics est = episodios.stream()
+		.filter(e -> e.getAvaliacao() > 0.0)
+		.collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+	System.out.println("Média: " + est.getAverage());
+	System.out.println("Melhor episódio: " + est.getMax());
+	System.out.println("Pior episódio: " + est.getMin());
+	System.out.println("Quantidade: " + est.getCount());
     }
 }
